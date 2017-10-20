ce7571678616a5679e86f93d21cacaab1fe9ef4a
Austin Cheng <austinbicheng@gmail.com>
2017-10-20 13:17:38 -0700

    1-6

--- src/euler/Problem6.java
+++ src/euler/Problem6.java
@@ -2,5 +2,25 @@ package euler;
 
 public class Problem6
 {
-
+	public Problem6()
+	{
+		int one = 0;
+		int two = 0;
+		for (int i = 1; i <= 100; i++)
+		{
+			one = one + i * i;
+		}
+		for (int i = 1; i <= 100; i++)
+		{
+			two = two + i;
+		}
+		two = two * two;
+		two = two - one;
+		System.out.println(two);
+	}
+	
+	public static void main(String args[])
+	{
+		new Problem6();
+	}
 }
